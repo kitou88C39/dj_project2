689408438325ba14741642578e369b32e2152b73
kitou88C39 <kanfutrooper@icloud.com>
2024-02-09 09:05:32 +0900

    edit models.py

--- project2/employee/models.py
+++ project2/employee/models.py
@@ -12,3 +12,8 @@ class Employee(models.Model):
     last_name = models.CharField('姓', max_lemgth=20)
     email = models.EmailField('メールアドレス', blank=True)
     Deparment, verbose_name(= '部署', on_delete=models.PRPJECT,)
+
+    created_at = models.DateTimeField('日付', default=timezone.now)
+
+    def__str(self):
+        return '{0} {1} {2}'.format(self.last_name, self.first_name, self.deparment)
