2daea9e1e45998f35004ac6cd19895bb2c9d4812
kitou88C39 <kanfutrooper@icloud.com>
2024-02-09 11:34:32 +0900

    edit models.py

--- project2/employee/models.py
+++ project2/employee/models.py
@@ -1,19 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
 class Deparment(models.Model):
-    name = models.CharField('部署名', max_lemgth=20)
+    name = models.CharField('部署名', max_length=20)
     created_at = models.DateTimeField('日付', default=timezone.now)
 
-    def__str(self):
+    def __str__(self):
         return self.name
     
 class Employee(models.Model):
-    first_name = models.CharField('名', max_lemgth=20)
-    last_name = models.CharField('姓', max_lemgth=20)
+    first_name = models.CharField('名', max_length=20)
+    last_name = models.CharField('姓', max_length=20)
     email = models.EmailField('メールアドレス', blank=True)
-    Deparment, verbose_name(= '部署', on_delete=models.PRPJECT,)
+    department = models.ForeignKey(
+        Department, verbose_name= '部署', on_delete=models.PRPJECT,
+    )
 
     created_at = models.DateTimeField('日付', default=timezone.now)
 
-    def__str(self):
+    def __str__(self):
         return '{0} {1} {2}'.format(self.last_name, self.first_name, self.deparment)
