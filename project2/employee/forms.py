95e33ac374fb7b61945adfc27ad2b794897e258b
kitou88C39 <kanfutrooper@icloud.com>
2024-02-13 21:23:08 +0900

    Create employee/forms.py

--- project2/employee/forms.py
+++ project2/employee/forms.py
@@ -1,2 +1,10 @@
 from django import froms
 from .models import Club, Deparment
+
+class SearchForm(froms.Form):
+    club = forms.ModelChoiceField(
+        queryset=Club.object, label='サークル', required=False)
+    
+    department = forms.ModelChoiceField(
+        queryset=Deparment.objects, label='部署', required=False)
+    
